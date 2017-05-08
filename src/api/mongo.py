3e049221b38c7ec84e73e4f1cd4540ef3b84fcea
lypc <leiyishan@baixing.com>
2017-05-08 12:44:35 +0800

    单例

--- src/api/mongo.py
+++ src/api/mongo.py
@@ -3,7 +3,20 @@ import pymongo
 from pymongo import MongoClient
 
 
-class Mongo():
+class Singleton(type):
+
+    _instances = {}
+
+    def __call__(cls, *args, **kwargs):
+        if cls not in cls._instances:
+            cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
+        return cls._instances[cls]
+
+
+class Mongo(metaclass=Singleton):
+
+    # python2 metaclass
+    # __metaclass__ = Singleton
 
     def __init__(self, db_name, is_pro=1):
 
@@ -65,7 +78,12 @@ class Mongo():
 
 def test():
     a = Mongo('chaoge')
-    print(a.read('RecommendationAd').next())
+    b = Mongo('chaoge')
+    print(a == b)
+    print(a is b)
+    print(id(a))
+    print(id(b))
+    # print(a.read('RecommendationAd').next())
     # print([x for x in a.insert('RecommendationUserTagsOffline', [{'user_id': '3a64b7666eca18fa',
     #                                                              'tags': {u"服务": {u"家电维修": {u"空调维修": 2, u"葫芦岛": 2}}}}])])
 
