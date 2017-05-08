d5e0cf8ca9a40507cd92c671fbd25eb126ed64c8
lypc <leiyishan@baixing.com>
2017-05-08 11:21:18 +0800

    change api

--- src/api/mongo.py
+++ src/api/mongo.py
@@ -5,10 +5,32 @@ from pymongo import MongoClient
 
 class Mongo():
 
-    def __init__(self, db_name):
+    def __init__(self, db_name, is_pro=1):
 
-        self.read_db = MongoClient('mongodb://ro:ReadOnly@192.168.1.40:27017')[db_name]
-        self.write_db = MongoClient('mongodb://admin:SuperPower@192.168.1.40:27017')[db_name]
+        self.db_name = db_name
+
+        dev_read_uri = 'mongodb://ro:ReadOnly@192.168.1.40:27017'
+        dev_write_uri = 'mongodb://admin:SuperPower@192.168.1.40:27017'
+        pro_read_uri = 'mongodb://ro:ReadOnly@192.168.1.40:27017'
+        pro_write_uri = 'mongodb://admin:SuperPower@192.168.1.40:27017'
+
+        if is_pro:
+            self.read_uri = pro_read_uri
+            self.write_uri = pro_write_uri
+
+        else:
+            self.read_uri = dev_read_uri
+            self.write_uri = dev_write_uri
+
+    def connect(self):
+
+        self.read_db = MongoClient(self.read_uri)[self.db_name]
+        self.write_db = MongoClient(self.write_uri)[self.db_name]
+
+    def close_connect(self):
+
+        self.read_db.close()
+        self.write_db.close()
 
     def read(self, collect_name, search_json={}):
 
@@ -18,10 +40,28 @@ class Mongo():
     def insert(self, collect_name, data):
 
         if len(data) == 0:
-            return
+            return []
 
         query_obj = self.write_db[collect_name]
-        return query_obj.insert_many(data)
+        result = query_obj.insert_many(data)
+        return result.inserted_ids
+
+    def delete(self, collect_name, search_json={}):
+
+        if len(search_json) == 0:
+            return []
+
+        result = self.write_db[collect_name].remove(search_json)
+        return 'success'
+
+    def update(self, collect_name, key, update_list):
+
+        coll = self.write_db[collect_name]
+        for d in update_list:
+            coll.update({key: d[key]}, d)
+
+        return 'success'
+
 
 def test():
     a = Mongo('chaoge')
