10ececed775a651f71cd9a8d43ff93e61fb65172
kaya33 <360138359@qq.com>
2017-05-22 17:24:24 +0800

    .

--- archimedes/rec_server.py
+++ archimedes/rec_server.py
@@ -86,15 +86,16 @@ def fetch_batch_userrec(user_id,first_cat,second_cat,city=None,size=3):
         second_cat = second_cat.encode('utf-8')
         begin = datetime.datetime.now()
         kwdata = {"num": size,"city": city,"category": second_cat,"tag": "_".join([x[0] for x in tmp_list]),"weights":[x[1] for x in tmp_list],"days": 60}
-        user_profile = fetchKwData(kwdata)
-        if len(user_profile)<3:
+        user_profile_ad = fetchKwData(kwdata)
+        print user_profile_ad
+        if len(user_profile_ad)<size:
             kwdata = {"num": size,"city": city,"category": second_cat,"tag": "_".join([x[0] for x in tmp_list]),"weights":[x[1] for x in tmp_list],"days": 270}
-            user_profile = fetchKwData(kwdata)
+            user_profile_ad.extend(fetchKwData(kwdata))
         end = datetime.datetime.now()
         print "get ad_list by user tag cost time %s sec\n" % (end - begin)
     except Exception as e:
         log.error("获取用户画像失败, {}".format(e))
-        user_profile = []
+        user_profile_ad = []
 
     tmp_list = []
     for info_tuple in user_profile:
